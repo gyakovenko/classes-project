fecf6f2b8181493b7679869e95bc91c2b02be70a
Galina Yakovenko <gyakovenko@gmail.com>
2017-02-21 09:16:39 -0800

    Update

--- src/test/java/com/sqa/gy/PersonTest.java
+++ src/test/java/com/sqa/gy/PersonTest.java
@@ -1,26 +1,12 @@
-/**
- *   File Name: PersonTest.java<br>
- *
- *   Yakovenko, Galina<br>
- *   Created: Jan 21, 2017
- *
- */
-
 package com.sqa.gy;
 
 import org.junit.*;
 
-/**
- * PersonTest //ADDD (description of class)
- *
- * @author Yakovenko, Galina
- */
 public class PersonTest {
 
 	@Test
-	public void test() {
-		Person person1 = new Person("Oakland", 29, 'O', 50, true, "SQA ENG", "Galina");
-		System.out.println(person1);
+	public void PersonConstructorTest() {
+
 	}
 
 }
